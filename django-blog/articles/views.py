ad771ffca11326eb632b22cd5407fec2a15501a9
= <=>
2023-04-20 00:56:45 +0500

    Work from tutorial 11 to 18

--- django-blog/articles/views.py
+++ django-blog/articles/views.py
@@ -8,3 +8,8 @@ def article_list(request):
     articles = Article.objects.all().order_by('date')
     # return HttpResponse(articles[0].body)
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article':article})
